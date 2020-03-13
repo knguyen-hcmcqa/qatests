01111fed1cc1b95806ca485781c9709ad2aff565
tloan87 <tloan87@gmail.com>
2020-03-14 00:25:04 +0700

    changes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.BasePage;
 import pages.PageGenerator;
+
+import java.util.Properties;
 
 public class BaseTest {
     public WebDriver driver;
@@ -13,12 +16,18 @@ public class BaseTest {
     public PageGenerator page;
 
     @BeforeMethod
-    public void setup () {
-        System.setProperty("webdriver.chrome.driver", "D:\\AutomationTest\\qatests\\browsers\\chromedriver.exe");
+    public void setup() {
+        try {
+            Properties properties = new Properties();
+            properties.load(BasePage.class.getClassLoader()
+                    .getResourceAsStream("application.properties"));
+            System.setProperty("webdriver.chrome.driver", properties.getProperty("chrome.path"));
+        } catch (Exception ex) {
+        }
 
         driver = new ChromeDriver();
 
-        wait = new WebDriverWait(driver,15);
+        wait = new WebDriverWait(driver, 15);
 
         driver.manage().window().maximize();
 
@@ -26,7 +35,7 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void teardown () {
+    public void teardown() {
         driver.quit();
     }
 }
